27cc399cb9a50215c53c9418c0d5a2d4e27fcfa6
EliasObreque <els.obrq@gmail.com>
2020-07-09 15:44:34 -0400

    Identificacion y regresores

--- Python tools/MainP2.py
+++
@@ -1,9 +0,0 @@
-"""
-Created: 7/4/2020
-Autor: Elias Obreque Sepulveda
-email: els.obrq@gmail.com
-
-"""
-
-import numpy as np
-import matplotlib.pyplot as plt
--- Python tools/P2/P2Tools.py
+++
@@ -1,6 +0,0 @@
-"""
-Created: 7/4/2020
-Autor: Elias Obreque Sepulveda
-email: els.obrq@gmail.com
-
-"""
