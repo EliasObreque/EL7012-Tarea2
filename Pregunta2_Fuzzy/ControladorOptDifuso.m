c8e1fdd7cdd8e517ff39b7136050a07ba423bd78
MaibethSanchez <64100298+MaibethSanchez@users.noreply.github.com>
2020-07-17 12:58:50 -0400

    Pregunta 2 Modelo difuso M=N=1 OK

--- Pregunta2_Fuzzy/ControladorOptDifuso.m
+++ Pregunta2_Fuzzy/ControladorOptDifuso.m
@@ -1,6 +1,5 @@
 function [u] = ControladorOptDifuso(r,h,f)
-tm=10;
-A=10;
+A=2;
 B=0.01;
 N=5;
 M=5; %Se debe cumplir M<=N
@@ -13,26 +12,23 @@ nu=1;
 %Construcción del vector de regresores para el Modelo Difuso
 if exist('Xdatos.mat')
     load('Xdatos');
-    for i=ny:-1:2
-        Xdatos(i)=Xdatos(i-1);
-    end
-    Xdatos(1)=h;
-    for i=ny+nu:-1:ny+1
-        Xdatos(i)=Xdatos(i-1);
-    end
-    Xdatos(ny+1)=f;
+    cont=cont+1;
+    Xdatos=ActualizarRegresores(Xdatos,ny,nu,h,0);
+    Xdatos=ActualizarRegresores(Xdatos,ny,nu,f,1);
 else
     Xdatos(1,1:ny)=h;
     Xdatos(1,ny+1:ny+nu)=f;
+    cont=1;
 end
 
-save('Xdatos','Xdatos');
+save('Xdatos','Xdatos','cont');
 
+% if cont >ny
 %Optimización
-lb=zeros(1,M);
-ub=100 * ones(1, M);%**Vectores fila
-nvars = N;
-fun = @(x)myfunMD(x,r,h,f,tm,A,B,N,M,modeloDifuso,Xdatos,ny,nu);
+lb=zeros(1,M+1);
+ub=100 * ones(1, M+1);%**Vectores fila
+nvars = M+1;
+fun = @(x)myfunMD(x,r,A,B,N,M,modeloDifuso,Xdatos,ny,nu);
 
 %Algoritmo de Optimización Convencional
 % x0=zeros(1,nvars);
@@ -46,37 +42,62 @@ fun = @(x)myfunMD(x,r,h,f,tm,A,B,N,M,modeloDifuso,Xdatos,ny,nu);
 [F,fval,exitflag,output,population,scores] = ga(fun,nvars,[],[],[],[],lb,ub);
 
 u=F(1);
+% else
+%     u=0;
+% end
 end
 
-function [J] = myfunMD(F,r,h,f,tm,A,B,N,M,modeloDifuso,X,ny,nu)
+function [J] = myfunMD(F,r,A,B,N,M,modeloDifuso,X,ny,nu)
+minH=15;
+maxH=50;
+
+H(1)=ysim(X,modeloDifuso.a,modeloDifuso.b,modeloDifuso.g);
+if H(1)<minH %Restricción física del nivel en el estanque
+    H(1)=minH;
+elseif H(1)>maxH
+    H(1)=maxH;
+end
 
 J=0;
 for i=1:N
-    H(i)=ysim(X,modeloDifuso.a,modeloDifuso.b,modeloDifuso.g);
-    if H(i)<15 %Restricción física del nivel en el tanque
-        H(i)=15;
-    elseif H(i)>50
-        H(i)=50;
+    X=ActualizarRegresores(X,ny,nu,H(i),0); %Actualizar el valor estimado
+    
+    if i<=M+1 %En caso contrario se maniene la ultima acción de control estimada
+        X=ActualizarRegresores(X,ny,nu,F(i),1);
     end
-    if i<=M
-        J = J + A*(H(i)-r)^2+ B*F(i)^2;        
-    else %Se maniene la ultima accion de control estimada
-        J = J + A*(H(i)-r)^2+ B*F(M)^2;        
+    
+    H(i+1)=ysim(X,modeloDifuso.a,modeloDifuso.b,modeloDifuso.g);
+    if H(i+1)<minH %Restricción física del nivel en el estanque
+        H(i+1)=minH;
+    elseif H(i+1)>maxH
+        H(i+1)=maxH;
     end
-    %Actualizar el valor Estimado
-    for j=ny:-1:2
-        X(j)=X(j-1);
+    if i<=M+1
+        J = J + A*(H(i+1)-r)^2+ B*F(i)^2;
+    else %Se maniene la ultima acción de control estimada
+        J = J + A*(H(i+1)-r)^2+ B*F(M+1)^2;
     end
-    X(1)=H(i);
-    if nu>1
-    for j=ny+nu:-1:ny+1
-        X(j)=X(j-1);
-    end
-    end
-    if i<=M
-        X(ny+1)=F(i);
-    else %Se maniene la ultima accion de control estimada
-        X(ny+1)=F(M);      
-    end    
+end
+end
+
+function[X] = ActualizarRegresores(X,ny,nu,xnew,tr)
+% tr    Tipo de regresor 0 Autorregresor
+%                        1 Regresor
+
+%Actualizar el valor en el vector de regresores
+switch tr
+    case 0
+        for j=ny:-1:2
+            X(j)=X(j-1);
+        end
+        X(1)=xnew;
+        
+    case 1
+        if nu>1
+            for j=ny+nu:-1:ny+1
+                X(j)=X(j-1);
+            end
+        end
+        X(ny+1)=xnew;
 end
 end
--- Pregunta2_Fuzzy/ControladorOptEuler.m
+++ Pregunta2_Fuzzy/ControladorOptEuler.m
@@ -2,13 +2,13 @@ function [u] = ControladorOptEuler(r,h,f)
 tm=10;
 A=2;
 B=0.01;
-N=5;
-M=5; %Se debe cumplir M<=N
+N=6;
+M=2; %Se debe cumplir M<=N
 
 %Optimización
-lb=zeros(1,M);
-ub=100 * ones(1, M);%**Vectores fila
-nvars = M;
+lb=zeros(1,M+1);
+ub=100 * ones(1, M+1);%**Vectores fila
+nvars = M+1;
 
 % Aproximación de Euler
 fun = @(x)myfunEuler(x,r,h,f,tm,A,B,N,M);
@@ -18,24 +18,31 @@ u=F(1);
 end
 
 function [J] = myfunEuler(F,r,h,f,tm,A,B,N,M)
+minH=15;
+maxH=50;
 
 H(1)=(5.43*f+78.23-20.21*sqrt(h))/(0.63*h^2+11.4*h+17.1)*tm + h;
+if H(1)<minH %Restricción física del nivel en el estanque
+    H(1)=minH;
+elseif H(1)>maxH
+    H(1)=maxH;
+end
 
 J=0;
 for i=1:N
-    if i<=M
-        J = J + A*(H(i)-r)^2+ B*F(i)^2;
-        H(i+1)=(5.43*F(i)+78.23-20.21*sqrt(H(i)))/(0.63*(H(i))^2+11.4*H(i)+17.1)*tm + H(i);
+    if i<=M+1
+        F_actual=F(i);
     else %Se maniene la ultima accion de control estimada
-        J = J + A*(H(i)-r)^2+ B*F(M)^2;
-        H(i+1)=(5.43*F(M)+78.23-20.21*sqrt(H(i)))/(0.63*(H(i))^2+11.4*H(i)+17.1)*tm + H(i);
+        F_actual=F(M+1);
     end
-    if H(i+1)<15 
-        H(i+1)=15;
-    elseif H(i+1)>50
-        H(i+1)=50;
+    
+    H(i+1)=(5.43*F_actual+78.23-20.21*sqrt(H(i)))/(0.63*(H(i))^2+11.4*H(i)+17.1)*tm + H(i);
+    if H(i+1)<minH %Restricción física del nivel en el estanque
+        H(i+1)=minH;
+    elseif H(i+1)>maxH
+        H(i+1)=maxH;
     end
+    J = J + A*(H(i+1)-r)^2+ B*F_actual^2;
 end
-
 end
 
