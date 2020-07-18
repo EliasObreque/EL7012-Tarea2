c31cdfe75e3b0e6ea0a8921b2cd9e0a2e8d7dbf6
EliasObreque <els.obrq@gmail.com>
2020-07-18 14:36:36 -0400

    cosas

--- Pregunta2_Fuzzy/ControladorOptDifuso.m
+++ Pregunta2_Fuzzy/ControladorOptDifuso.m
@@ -5,14 +5,14 @@ B=0.01;
 N=5;
 M=5; %Se debe cumplir M<=N
 
-%Modelo Difuso
-modeloDifuso=load('ModeloDifuso.mat');
-ny=3;
-nu=1;
+% Modelo neuronal
+ny = best_auto_reg;
+nu = best_reg;
+modelo = net_trained;
 
-%Construcción del vector de regresores para el Modelo Difuso
-if exist('Xdatos.mat')
-    load('Xdatos');
+%Construcción del vector de regresores
+if exist('XDatos.mat', 'file')
+    load('XDatos.mat');
     for i=ny:-1:2
         Xdatos(i)=Xdatos(i-1);
     end
@@ -26,33 +26,34 @@ else
     Xdatos(1,ny+1:ny+nu)=f;
 end
 
-save('Xdatos','Xdatos');
+save('XDatos','Xdatos');
 
 %Optimización
-lb=zeros(1,M);
-ub=100 * ones(1, M);%**Vectores fila
+lb = zeros(1,M);
+ub = 100 * ones(1, M);%**Vectores fila
 nvars = N;
-fun = @(x)myfunMD(x,r,h,f,tm,A,B,N,M,modeloDifuso,Xdatos,ny,nu);
+fun = @(x)myfunMNN(x,r,h,f,tm,A,B,N,M, modelo, Xdatos,ny,nu);
 
-%Algoritmo de Optimización Convencional
-% x0=zeros(1,nvars);
-% [F,fval] = fmincon(fun,x0,[],[],[],[],lb,ub);
+%% Algoritmo de Optimización Convencional
+x0=zeros(1,nvars);
+[F,fval] = fmincon(fun,x0,[],[],[],[],lb,ub);
 
-% %PSO
+%% PSO
 % options = optimoptions('particleswarm','SwarmSize',40,'MaxIterations',100);
 % [F,fval,exitflag,output]= particleswarm(fun, nvars, lb, ub,options);
 
-% GA
-[F,fval,exitflag,output,population,scores] = ga(fun,nvars,[],[],[],[],lb,ub);
+%% GA
+%[F,fval,exitflag,output,population,scores] = ga(fun,nvars,[],[],[],[],lb,ub);
 
 u=F(1);
 end
 
-function [J] = myfunMD(F,r,h,f,tm,A,B,N,M,modeloDifuso,X,ny,nu)
+function [J] = myfunNN(F,r,h,f,tm,A,B,N,M,modelo,X,ny,nu)
 
 J=0;
 for i=1:N
-    H(i)=ysim(X,modeloDifuso.a,modeloDifuso.b,modeloDifuso.g);
+    % H(i)=ysim(X,modeloDifuso.a,modeloDifuso.b,modeloDifuso.g);
+    H(i) = predictiveNN(M, modelo, x_train, x_test, x_val); 
     if H(i)<15 %Restricción física del nivel en el tanque
         H(i)=15;
     elseif H(i)>50
