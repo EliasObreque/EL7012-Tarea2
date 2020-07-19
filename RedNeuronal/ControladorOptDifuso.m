598e81531cfeaa2e13188f6eec43de094dbe04cf
EliasObreque <els.obrq@gmail.com>
2020-07-19 16:25:54 -0400

    Graficos

--- RedNeuronal/ControladorOptDifuso.m
+++ RedNeuronal/ControladorOptDifuso.m
@@ -1,8 +1,8 @@
 function [u] = ControladorOptDifuso(r,h,f)
-global reg_count best_auto_reg best_reg net_trained
+global reg_count best_auto_reg best_reg net_trained counter_time time_prom
 tm=10;
-A=10;
-B=0.01;
+A=2;
+B=0.001;
 N=5;
 M=5; %Se debe cumplir M<=N
 
@@ -37,53 +37,81 @@ if reg_count < ny
     u = 0;
     reg_count = reg_count + 1;
 else
+    tinit = cputime; 
     fun = @(x)myfunMNN(x,r,h,f,tm,A,B,N,M, modelo, Xdatos,ny,nu);
 
     %% Algoritmo de Optimización Convencional
-%     x0=zeros(1,nvars);
-%     [F,fval] = fmincon(fun,x0,[],[],[],[],lb,ub);
-
+    %x0=zeros(1,nvars);
+    %options = optimoptions(@fmincon,'Display','off','MaxIterations',100,'StepTolerance',1e-3,'OptimalityTolerance',1e-3);
+    %F = fmincon(fun,x0,[],[],[],[],lb,ub,[],options);
     %% PSO
-      options = optimoptions('particleswarm','SwarmSize',40,'MaxIterations',100);
-      [F,fval,exitflag,output]= particleswarm(fun, nvars, lb, ub,options);
+    %options = optimoptions('particleswarm','SwarmSize',20,'MaxIterations', 10);
+    %options = optimoptions('particleswarm','SwarmSize',20,'MaxIterations', 30);
+    %[F,fval,exitflag,output]= particleswarm(fun, nvars, lb, ub,options);
      %% GA
-%    [F,fval,exitflag,output,population,scores] = ga(fun,nvars,[],[],[],[],lb,ub);
+    options = optimoptions('ga','PopulationSize',20,'MaxGenerations', 10);
+    %options = optimoptions('ga','PopulationSize',40,'MaxGenerations', 30);
+    [F,fval,exitflag,output,population,scores] = ga(fun,nvars,[],[],[],[],lb,ub,[],[],options);
 
     u=F(1);
+    tout = cputime;
+    time_prom = time_prom + tout-tinit;
+    fprintf('Tiempo de cálculo %f1.2 \n', tout-tinit)
+    counter_time = counter_time + 1;
 end
 end
 
 function [J] = myfunMNN(F,r,h,f,tm,A,B,N,M,modelo,X,ny,nu)
+H(1) = modelo(X'); 
 
+if H(1)<15 %Restricción física del nivel en el tanque
+    H(1)=15;
+elseif H(1)>50
+    H(1)=50;
+end
+    
 J=0;
-for i=1:N
-    % H(i)=ysim(X,modeloDifuso.a,modeloDifuso.b,modeloDifuso.g);
-    H(i) = modelo(X'); 
+%itera = itera + 1
 
-    if H(i)<15 %Restricción física del nivel en el tanque
-        H(i)=15;
-    elseif H(i)>50
-        H(i)=50;
+for i=1:N
+    X=ActualizarRegresores(X,ny,nu,H(i),0); %Actualizar el valor estimado
+    
+    if i<=M+1 %En caso contrario se maniene la ultima acción de control estimada
+        X=ActualizarRegresores(X,ny,nu,F(i),1);
     end
-    if i<=M
-        J = J + A*(H(i)-r)^2+ B*F(i)^2;        
-    else %Se maniene la ultima accion de control estimada
-        J = J + A*(H(i)-r)^2+ B*F(M)^2;        
+    
+    H(i+1) = modelo(X');
+    if H(i+1)<15 %Restricción física del nivel en el tanque
+        H(i+1)=15;
+    elseif H(i+1)>50
+        H(i+1)=50;
     end
-    %Actualizar el valor Estimado
-    for j=ny:-1:2
-        X(j)=X(j-1);
+    if i<=M+1
+        J = J + A*(H(i+1)-r)^2+ B*F(i)^2;
+    else %Se maniene la ultima acción de control estimada
+        J = J + A*(H(i+1)-r)^2+ B*F(M+1)^2;
     end
-    X(1)=H(i);
-    if nu>1
-    for j=ny+nu:-1:ny+1
-        X(j)=X(j-1);
-    end
-    end
-    if i<=M
-        X(ny+1)=F(i);
-    else %Se maniene la ultima accion de control estimada
-        X(ny+1)=F(M);      
-    end    
+end
+end
+
+function[X] = ActualizarRegresores(X,ny,nu,xnew,tr)
+% tr    Tipo de regresor 0 Autorregresor
+%                        1 Regresor
+
+%Actualizar el valor en el vector de regresores
+switch tr
+    case 0
+        for j=ny:-1:2
+            X(j)=X(j-1);
+        end
+        X(1)=xnew;
+        
+    case 1
+        if nu>1
+            for j=ny+nu:-1:ny+1
+                X(j)=X(j-1);
+            end
+        end
+        X(ny+1)=xnew;
 end
 end
--- RedNeuronal/ModelPredictiveControlNN.m
+++ RedNeuronal/ModelPredictiveControlNN.m
@@ -3,22 +3,15 @@
 clc
 clear all
 close all
-global best_auto_reg best_reg net_trained
-
+global best_auto_reg best_reg net_trained time_prom counter_time
+time_prom = 0;
+counter_time = 1;
 best_regressor_mat = 'best_regressor.mat';
 best_hidden_neurons_mat = 'best_hidden_neurons.mat';
 NN_model_mat = 'NN_model.mat';
 %% Regressors Selection
 
 load(best_regressor_mat)
-
-x_train = best_X.x_train;
-x_test = best_X.x_test;
-x_val = best_X.x_val;
-
-y_train = best_Y.y_train;
-y_test = best_Y.y_test;
-y_val = best_Y.y_val;
 
 clear best_X best_Y num_neu best_train_rmse
 %% Optimal hidden neurons
@@ -28,16 +21,16 @@ load(best_hidden_neurons_mat)
 
 load(NN_model_mat)
 
-
 tescalon=180;
-tsim=tescalon*6;
+tsim=tescalon*5;
 t1 = cputime;
 sim('SimEstanqueNN',tsim)
 t2 = cputime;
-disp(t2-t1)
+fprintf('Tiempo de simulación: %f0.1 \n', t2-t1)
+fprintf('Tiempo de cálculo promedio: %f0.1 \n', time_prom/counter_time)
 %%
 
-e=RMSE(h(:,1),h(:,2))
+e = RMSE(h(:,1),h(:,2))
 
 figure ()
 subplot(2,1,1), plot (tout,h(:,1))
